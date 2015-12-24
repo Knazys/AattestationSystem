cdaac5c636505fe6a429767dc58217c4591e90e1
Knazys <knazys@gmail.com>
2015-12-24 16:36:28 +0300

    Добавлен 1 тест

---
+++ AattestationSystem/Controllers/TestJSController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AattestationSystem.Controllers
+{
+    public class TestJSController : Controller
+    {
+        // GET: TestJS
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Test1()
+        {
+            return View();
+        }
+    }
+}
---
+++ AattestationSystem/Scripts/TestJS/test1.js
@@ -0,0 +1,22 @@
+﻿
+
+$(document).ready(function ()
+{
+    var interval = setInterval(refreshAsyncContainer, 1000);
+
+
+});
+
+
+refreshAsyncContainer = function ()
+{
+    var asyncContainer = $('.async-container');
+    asyncContainer.html('');
+
+    var date = new Date().toLocaleString();
+    var p = $('<p>').html('Местное время '+date);
+    asyncContainer.append(p);
+
+    var button = $('<button>').addClass('btn btn-primary').html('Вывести текст');
+    asyncContainer.append(button);
+};
